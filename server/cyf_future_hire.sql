87f9aeb82e4a003cdaad3410aa48640d168e3c1a
kavitappatil <patil.poem@gmail.com>
2023-02-15 18:43:33 +0000

    updated schema

--- server/cyf_future_hire.sql
+++ server/cyf_future_hire.sql
@@ -1,19 +1,19 @@
 
 drop table if exists skills;
-drop table if exists graduates;
-drop table if exists graduates_account;
+drop table if exists students;
+drop table if exists students_account;
 
-CREATE TABLE graduates_account 
+CREATE TABLE students_account 
 (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     pwd VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE graduates 
+CREATE TABLE students 
 (
     id SERIAL PRIMARY KEY,
-    graduate_account_id INT REFERENCES graduates_account(id),
+    student_account_id INT REFERENCES students_account(id),
     full_name VARCHAR(120) NOT NULL,
     cohort VARCHAR(200) NOT NULL,
     passing_year date NOT NULL,
@@ -27,131 +27,131 @@ CREATE TABLE graduates
 CREATE TABLE skills 
 (
     id SERIAL PRIMARY KEY,
-    graduate_id INT REFERENCES graduates(id),
+    student_id INT REFERENCES students(id),
     skill VARCHAR(50) NOT NULL
 );
 
-INSERT INTO graduates_account
+INSERT INTO students_account
         (email,pwd)
 VALUES ('kavita.123@gmail.com', 'password@1');
-INSERT INTO graduates_account
+INSERT INTO students_account
         (email,pwd)
 VALUES ('franklin.123@gmail.com', 'password@1');
-INSERT INTO graduates_account
+INSERT INTO students_account
         (email,pwd)
 VALUES ('simeon.123@gmail.com', 'password@1');
-INSERT INTO graduates_account
+INSERT INTO students_account
         (email,pwd)
 VALUES ('brolin123@gmail.com', 'password@1');
-INSERT INTO graduates_account
+INSERT INTO students_account
         (email,pwd)
 VALUES ('emeka123@gmail.com', 'password@1');
 
 
-INSERT INTO graduates
-    (graduate_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
+INSERT INTO students
+    (student_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
 VALUES
     (1,'Kavita Patil', 'West Midlands', '2023-03-31','kavita.123@gmail.com',079826308100,'Fullstack Developer','female');
-INSERT INTO graduates
-    (graduate_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
+INSERT INTO students
+    (student_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
 VALUES
     (2,'Franklin', 'West Midlands', '2023-03-31','franklin.123@gmail.com',079826308200,'Frontend Developer','male');
-INSERT INTO graduates
-    (graduate_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
+INSERT INTO students
+    (student_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
 VALUES
     (3,'Simeon', 'West Midlands', '2023-03-31','simeon.123@gmail.com',079826308300,'Fullstack Developer','male');
-INSERT INTO graduates
-    (graduate_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
+INSERT INTO students
+    (student_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
 VALUES
     (4,'Brolin', 'West Midlands', '2023-03-31','brolin123@gmail.com',079826308400,'Frontend Developer','male');
-INSERT INTO graduates
-    (graduate_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
+INSERT INTO students
+    (student_account_id,full_name,cohort,passing_year,email,mobile,professional_interest,gender)
 VALUES
     (5,'Emeka', 'West Midlands', '2023-03-31','emeka123@gmail.com',079826308500,'Frontend Developer','male');
 
 
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (1, 'HTML5');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (1, 'CSS');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (1, 'JAVASCRIPT');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (1, 'NODE');
 
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (2, 'HTML5');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (2, 'CSS');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (2, 'JAVASCRIPT');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (2, 'NODE');
 
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (3, 'HTML5');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (3, 'CSS');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (3, 'JAVASCRIPT');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (3, 'NODE'); 
 
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (4, 'HTML5');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (4, 'CSS');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (4, 'JAVASCRIPT');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (4, 'NODE');  
 
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (5, 'HTML5');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (5, 'CSS');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (5, 'JAVASCRIPT');
 INSERT INTO skills
-    (graduate_id, skill)
+    (student_id, skill)
 VALUES
     (5, 'NODE');           
 
