2673b0f4d7c04a6949653ac22d5a43d6b1a84a01
simeonbikov <bikovsem@gmail.com>
2023-02-16 00:20:19 +0000

    added new endpoints: /students & /students/id

--- server/api.js
+++ server/api.js
@@ -1,12 +1,38 @@
 import { Router } from "express";
-
 import logger from "./utils/logger";
+import db from "./db";
 
 const router = Router();
 
 router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
 	res.json({ message: "Hello, world!" });
+});
+
+// GET "/students"
+router.get("/students", async (_, res) => {
+	const query = "SELECT * FROM students";
+	try {
+		const students = await db.query(query);
+		res.json(students.rows);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+// GET "/students/{id}"
+router.get("/students/:id", async (req, res) => {
+	const studentId = parseInt(req.params.id);
+	const query = "SELECT * FROM students WHERE id = $1";
+	try {
+		const student = await db.query(query, [studentId]);
+		if (student.rows.length <= 0) {
+			res.sendStatus(404);
+		}
+		res.json(student.rows);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 export default router;
