545f274cf9f0a91975517785e598d337d3d502a8
Emeka1993 <brolin933@gmail.com>
2023-03-01 18:31:04 +0000

    Fixed NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,16 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Graduates from "./pages/Graduates";
 import Layout from "./Components/Layout";
-import Hero from "../src/Components/Hero";
+import ContactForm from "./pages/Form";
 
 const App = () => {
   return (
     <Layout>
-      <Hero
-        title="Hire a Graduate"
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/graduates/:id" element={<Graduates />} />
         <Route path="/about/this/site" element={<About />} />
+        <Route path="/Contact-Us" element={<ContactForm />} />
       </Routes>
     </Layout>
   );
