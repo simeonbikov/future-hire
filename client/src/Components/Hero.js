42b50e8bb3d4ddba344c21a714d88dbcc408ec1a
simeonbikov <bikovsem@gmail.com>
2023-03-07 18:37:35 +0000

    in package-lock deleted old dependencies

--- client/src/Components/Hero.js
+++ client/src/Components/Hero.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "../Components/Hero.css";
 import heroImage from "../Images/cyfteam.jpeg";
-import Typed from "react-typed";
+// import Typed from "react-typed";
 
 function Hero(props) {
-  const titleOptions = {
-    strings: [props.title],
-    typeSpeed: 80,
-    backSpeed: 80,
-    loop: true,
-    loopCount: 2,
-  };
+  // const titleOptions = {
+  //   strings: [props.title],
+  //   typeSpeed: 80,
+  //   backSpeed: 80,
+  //   loop: true,
+  //   loopCount: 2,
+  // };
 
   return (
-    <div className="hero-container">
-      <img src={heroImage} alt="cyf-team" className="hero-image" />
-      <div className="hero-content">
-        <Typed {...titleOptions} />
-      </div>
-    </div>
-  );
+		<div className="hero-container">
+			<img src={heroImage} alt="cyf-team" className="hero-image" />
+			<div className="hero-content">
+				{/* <Typed {...titleOptions} /> */}
+				<h1>{props.title}</h1>
+			</div>
+		</div>
+	);
 }
 
 export default Hero;
