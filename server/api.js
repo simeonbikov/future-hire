7c212d5f63497e7d97e2e184b48082b524247009
simeonbikov <bikovsem@gmail.com>
2023-03-10 01:53:36 +0000

    chenge coep

--- server/api.js
+++ server/api.js
@@ -5,7 +5,7 @@ import db from "./db";
 const router = Router();
 
 router.use(function (req, res, next) {
-	// res.header("Cross-Origin-Embedder-Policy", "require-corp");
+	res.header("Cross-Origin-Embedder-Policy", "require-corp");
 	res.header("Cross-Origin-Opener-Policy", "same-origin");
 	next();
 });
