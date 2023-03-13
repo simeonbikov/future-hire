452c47b25088e4e2a9e468ea4c9f2274a022e9cd
kavitappatil <patil.poem@gmail.com>
2023-03-13 16:48:20 +0000

    Edit graduate form completed

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,22 @@ import Home from "./pages/Home";
 import Graduates from "./pages/Graduates";
 import Layout from "./Components/Layout";
 import ContactUs from "./pages/ContactUs";
-import AddGraduateForm from "./Components/AddGraduateForm";
+import AddGraduateForm from "./pages/AddGraduateForm";
+import EditAddGraduateForm from "./pages/EditAddGraduateForm";
 
 const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/graduates/:id" element={<Graduates />} />
-        <Route path="/about/this/site" element={<About />} />
-        <Route path="/Contact-Us" element={<ContactUs />} />
-        <Route path="/register" element={<AddGraduateForm />} />
-      </Routes>
-    </Layout>
-  );
+		<Layout>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/graduates/:id" element={<Graduates />} />
+				<Route path="/about/this/site" element={<About />} />
+				<Route path="/Contact-Us" element={<ContactUs />} />
+				<Route path="/register" element={<AddGraduateForm />} />
+				<Route path="/updateProfile/:id" element={<EditAddGraduateForm />} />;
+			</Routes>
+		</Layout>
+	);
 };
 
 export default App;
